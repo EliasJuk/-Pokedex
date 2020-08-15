7d8fc47808efee7fca19816860a2dca6fb7d000f
Elias Juk <eliasp.juk@hotmail.com>
2020-08-15 17:08:47 -0300

    Carregar dados e sprits

--- pokedex/src/components/Pokemon.vue
+++ pokedex/src/components/Pokemon.vue
@@ -1,12 +1,45 @@
 <template>
-  <div>
-    <h1>{{num}} {{name | upper}}</h1>
-    <small>{{url}}</small>
-  </div>
+  <div id="pokemon">
+    <div class="card">
+      <div class="card-image">
+        <figure>
+          <img :src="pokemon.front" alt="Placeholder image">
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">{{num}} - {{name | upper}}</p>
+            <p class="subtitle is-6">{{pokemon.type}}</p>
+          </div>
+        </div>
+        <div class="content">
+        </div>
+      </div>
+    </div>
+</div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
+  created: function(){
+    axios.get(this.url).then(res => {
+      this.pokemon.type = res.data.types[0].type.name;
+      this.pokemon.front = res.data.sprites.front_default;
+      this.pokemon.back = res.data.sprites.back_default;
+      console.log(this.pokemon)
+    })
+  },
+  data(){
+    return{
+      pokemon: {
+        type: '',
+        front: '',
+        back: ''
+      }
+    }
+  },
   props: {
     num: Number,
     name: String,
@@ -22,5 +55,7 @@ export default {
 </script>
 
 <style>
-
+  #pokemon{
+    margin-top: 2%;
+  }
 </style>
