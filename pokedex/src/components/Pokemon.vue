2e1951c4c540a98d94b1adc3f461c7c2992a01f3
Elias Juk <eliasp.juk@hotmail.com>
2020-08-15 17:20:14 -0300

    Mudar sprits

--- pokedex/src/components/Pokemon.vue
+++ pokedex/src/components/Pokemon.vue
@@ -3,7 +3,7 @@
     <div class="card">
       <div class="card-image">
         <figure>
-          <img :src="pokemon.front" alt="Placeholder image">
+          <img :src="currentImg" alt="Placeholder image">
         </figure>
       </div>
       <div class="card-content">
@@ -14,6 +14,7 @@
           </div>
         </div>
         <div class="content">
+          <button class="button is-medium is-fullwidth" @click="mudarSprite">Mudar Sprit</button>
         </div>
       </div>
     </div>
@@ -28,11 +29,14 @@ export default {
       this.pokemon.type = res.data.types[0].type.name;
       this.pokemon.front = res.data.sprites.front_default;
       this.pokemon.back = res.data.sprites.back_default;
+      this.currentImg = this.pokemon.front;
       console.log(this.pokemon)
     })
   },
   data(){
     return{
+      isFront: true,
+      currentImg: '',
       pokemon: {
         type: '',
         front: '',
@@ -49,6 +53,17 @@ export default {
     upper: function(value){
       var newName = value[0].toUpperCase() + value.slice(1);
       return newName;
+    }
+  },
+  methods: {
+    mudarSprite: function() {
+      if(this.isFront){
+        this.isFront = false;
+        this.currentImg = this.pokemon.back;
+      }else{
+        this.isFront = true;
+        this.currentImg = this.pokemon.front;
+      }
     }
   }
 }
