bfd58e8faaf2df4e207ac1d494aadb29f4fb4466
Elias Juk <eliasp.juk@hotmail.com>
2020-08-16 17:01:59 -0300

    Sistema de busca

--- pokedex/src/App.vue
+++ pokedex/src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
     <div class="column is-half is-offset-one-quarter">
-      <div v-for="(poke,index) in pokemons" :key="index">
+      <hr/>
+      <h1 class="is-size-4">POKEDEX</h1>
+      <hr/>
+      <input class="input is-rounded" type="text" placeholder="Buscar pokemon pelo nome" v-model="busca">
+      <button class="button is-success is-fullwidth" id="buscaBtn" @click="buscar">Buscar</button>
+      <hr/>
+      <div v-for="(poke,index) in filteredPokemons" :key="poke.url">
         <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
       </div>
     </div>
   </div>
 </template>
-
 <script>
   import axios from 'axios';
   import Pokemon from './components/Pokemon';
@@ -15,18 +20,42 @@
     name: 'App',
     data(){
       return {
-        pokemons: []
+        pokemons: [],
+        filteredPokemons: [],
+        busca: ''
       }
     },
     created: function(){
       axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res => {
         console.log("Pegou a lista de pokemons");
         this.pokemons = res.data.results;
+        this.filteredPokemons = res.data.results;
       })
     },
     components:{
       Pokemon
+    },
+    methods:{
+      buscar: function(){
+        this.filteredPokemons = this.pokemons;
+        if(this.busca == '' || this.busca == ' '){
+          this.filteredPokemons = this.pokemons;
+        }else{
+          this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.busca);
+        }
+      }
     }
+    /*
+    computed:{
+      resultadoBusca: function(){
+        if(this.busca == '' || this.busca == ' '){
+          return this.pokemons;
+        }else{
+          return this.pokemons.filter(pokemon => pokemon.name == this.busca);
+        }
+      }
+    }
+    */
   }
 </script>
 
@@ -37,6 +66,8 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#buscaBtn {
+  margin-top: 2%;
 }
 </style>
